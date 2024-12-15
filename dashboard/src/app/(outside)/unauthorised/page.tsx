faa3485ca87d8932107e1e22414b73cf4fd80479
Markus Thielker <mail.markus.thielker@gmail.com>
2024-12-15 16:37:33 +0100

    NORY-36: add logout button to /unauthorised

--- dashboard/src/app/(outside)/unauthorised/page.tsx
+++ dashboard/src/app/(outside)/unauthorised/page.tsx
@@ -1,11 +1,20 @@
-import { ErrorDisplay } from '@/components/error';
+'use client';
 
-export default async function UnauthorizedPage() {
+import { ErrorDisplay } from '@/components/error';
+import { Button } from '@/components/ui/button';
+import { LogoutLink } from '@/ory';
+import { LogOut } from 'lucide-react';
+
+export default function UnauthorizedPage() {
     return (
         <div className="bg-background min-h-screen p-16">
             <ErrorDisplay
                 title="Unauthorised"
                 message="You are unauthorised to access this application!"/>
+            <Button className="mt-8 space-x-2" onClick={LogoutLink()}>
+                <LogOut className="h-4 w-4"/>
+                <span>Logout</span>
+            </Button>
         </div>
     );
 }
