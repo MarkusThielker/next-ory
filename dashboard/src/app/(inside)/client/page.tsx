6335036a0421f44ebef42391a6af47273d00996f
Markus Thielker <mail.markus.thielker@gmail.com>
2024-12-27 17:17:46 +0100

    NORY-34: show oauth2 clients in a data table

--- dashboard/src/app/(inside)/client/page.tsx
+++ dashboard/src/app/(inside)/client/page.tsx
@@ -1,12 +1,47 @@
+import { getOAuth2Api } from '@/ory/sdk/server';
+import { parseTokens } from '@/app/(inside)/user/page';
+import { ClientDataTable } from '@/app/(inside)/client/data-table';
+
+export interface FetchClientPageProps {
+    pageSize: number;
+    pageToken: string;
+}
+
+async function fetchClientPage({ pageSize, pageToken }: FetchClientPageProps) {
+    'use server';
+
+    const oAuth2Api = await getOAuth2Api();
+    const response = await oAuth2Api.listOAuth2Clients({
+        pageSize: pageSize,
+        pageToken: pageToken,
+    });
+
+    return {
+        data: response.data,
+        tokens: parseTokens(response.headers.link),
+    };
+}
+
 export default async function ListClientPage() {
+
+    let pageSize = 100;
+    let pageToken: string = '00000000-0000-0000-0000-000000000000';
+
+    const initialFetch = await fetchClientPage({ pageSize, pageToken });
+
     return (
         <div className="space-y-4">
             <div>
                 <p className="text-3xl font-bold leading-tight tracking-tight">OAuth2 Clients</p>
                 <p className="text-lg font-light">
-                    Part of milestone v0.2.0
+                    See and manage all OAuth2 clients registered with your Ory Hydra instance
                 </p>
             </div>
+            <ClientDataTable
+                data={initialFetch.data}
+                pageSize={pageSize}
+                pageToken={initialFetch.tokens.get('next')}
+                fetchClientPage={fetchClientPage}/>
         </div>
     );
 }
