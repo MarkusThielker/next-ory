502098d8ef8e270fa1a3c35fb58cf3eb9a487103
Markus Thielker <mail.markus.thielker@gmail.com>
2024-12-27 17:10:38 +0100

    NORY-34: add missing placeholder pages

---
+++ dashboard/src/app/(inside)/client/page.tsx
@@ -0,0 +1,12 @@
+export default async function ListClientPage() {
+    return (
+        <div className="space-y-4">
+            <div>
+                <p className="text-3xl font-bold leading-tight tracking-tight">OAuth2 Clients</p>
+                <p className="text-lg font-light">
+                    Part of milestone v0.2.0
+                </p>
+            </div>
+        </div>
+    );
+}
