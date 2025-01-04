7df37e400188a3e66e096ac5d3764196b1b8f774
Markus Thielker <mail.markus.thielker@gmail.com>
2025-01-04 10:53:08 +0100

    NORY-41: apply new query and temporarily remove pagination

--- dashboard/src/app/(inside)/user/page.tsx
+++ dashboard/src/app/(inside)/user/page.tsx
@@ -1,49 +1,7 @@
 import React from 'react';
 import { IdentityDataTable } from '@/app/(inside)/user/data-table';
-import { getIdentityApi } from '@/ory/sdk/server';
 import { SearchInput } from '@/components/search-input';
-
-export interface FetchIdentityPageProps {
-    pageSize: number;
-    pageToken: string;
-    query: string;
-}
-
-async function fetchIdentityPage({ pageSize, pageToken, query }: FetchIdentityPageProps) {
-    'use server';
-
-    const identityApi = await getIdentityApi();
-    const response = await identityApi.listIdentities({
-        pageSize: pageSize,
-        pageToken: pageToken,
-        previewCredentialsIdentifierSimilar: query,
-    });
-
-    return {
-        data: response.data,
-        tokens: parseTokens(response.headers.link),
-    };
-}
-
-function parseTokens(link: string) {
-
-    const parsed = link.split(',').map((it) => {
-        const startRel = it.lastIndexOf('rel="');
-        const endRel = it.lastIndexOf('"');
-        const rel = it.slice(startRel, endRel);
-
-        const startToken = it.lastIndexOf('page_token=');
-        const endToken = it.lastIndexOf('&');
-        const token = it.slice(startToken, endToken);
-
-        return [rel, token];
-    });
-
-    return new Map(parsed.map(obj => [
-        obj[0].replace('rel="', ''),
-        obj[1].replace('page_token=', ''),
-    ]));
-}
+import { queryIdentities } from '@/lib/action/identity';
 
 export default async function UserPage(
     {
@@ -54,12 +12,13 @@ export default async function UserPage(
 ) {
 
     const params = await searchParams;
+
+    const page = params.page ? Number(params.page) : 1;
     const query = params.query ? params.query as string : '';
 
     let pageSize = 250;
-    let pageToken: string = '00000000-0000-0000-0000-000000000000';
 
-    const initialFetch = await fetchIdentityPage({ pageSize, pageToken, query: query });
+    const initialData = await queryIdentities({ page, pageSize, query });
 
     return (
         <div className="space-y-4">
@@ -72,11 +31,9 @@ export default async function UserPage(
             <div className="space-y-2">
                 <SearchInput queryParamKey="query" placeholder="Search for identifiers (Email, Username...)"/>
                 <IdentityDataTable
-                    data={initialFetch.data}
-                    pageSize={pageSize}
-                    pageToken={initialFetch.tokens.get('next')}
-                    query={query}
-                    fetchIdentityPage={fetchIdentityPage}/>
+                    data={initialData}
+                    page={page}
+                    query={query}/>
             </div>
         </div>
     );
