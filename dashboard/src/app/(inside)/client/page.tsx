4ebd10d699ba2e281a1f1c1257fe376e8fe8eb4e
Markus Thielker <mail.markus.thielker@gmail.com>
2025-01-06 19:27:37 +0100

    NORY-34: add parseTokens() function after rebase

--- dashboard/src/app/(inside)/client/page.tsx
+++ dashboard/src/app/(inside)/client/page.tsx
@@ -1,10 +1,29 @@
 import { getOAuth2Api } from '@/ory/sdk/server';
-import { parseTokens } from '@/app/(inside)/user/page';
 import { ClientDataTable } from '@/app/(inside)/client/data-table';
 
 export interface FetchClientPageProps {
     pageSize: number;
     pageToken: string;
+}
+
+function parseTokens(link: string) {
+
+    const parsed = link.split(',').map((it) => {
+        const startRel = it.lastIndexOf('rel="');
+        const endRel = it.lastIndexOf('"');
+        const rel = it.slice(startRel, endRel);
+
+        const startToken = it.lastIndexOf('page_token=');
+        const endToken = it.lastIndexOf('&');
+        const token = it.slice(startToken, endToken);
+
+        return [rel, token];
+    });
+
+    return new Map(parsed.map(obj => [
+        obj[0].replace('rel="', ''),
+        obj[1].replace('page_token=', ''),
+    ]));
 }
 
 async function fetchClientPage({ pageSize, pageToken }: FetchClientPageProps) {
