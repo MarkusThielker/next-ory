5fb9170c81a99fed7ec1c8b6a80775841ea502fa
Markus Thielker <mail.markus.thielker@gmail.com>
2025-01-06 19:29:39 +0100

    NORY-34: fix milestone references on placeholder pages

--- dashboard/src/app/(inside)/analytics/page.tsx
+++ dashboard/src/app/(inside)/analytics/page.tsx
@@ -4,7 +4,7 @@ export default async function AnalyticsPage() {
             <div>
                 <p className="text-3xl font-bold leading-tight tracking-tight">Analytics</p>
                 <p className="text-lg font-light">
-                    Part of milestone v0.3.0
+                    Part of milestone v0.5.0
                 </p>
             </div>
         </div>
