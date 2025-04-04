6b80e93bf02b3e3ba9c41037dafc0c9545d6957a
Markus Thielker <hey@thielker.dev>
2025-04-04 19:32:26 +0200

    NORY-59: introduce permission constants

---
+++ dashboard/src/lib/permission.ts
@@ -0,0 +1,16 @@
+const permission = {
+    stack: {
+        status: 'admin.stack.status',
+    },
+    user: {
+        it: 'admin.user',
+        session: 'admin.user.session',
+        state: 'admin.user.state',
+    },
+};
+
+const relation = {
+    access: 'access',
+    edit: 'edit',
+    delete: 'delete',
+};
