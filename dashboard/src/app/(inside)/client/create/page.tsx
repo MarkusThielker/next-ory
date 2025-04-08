96b1af1ce0508c2b5586a834eff22f863d272358
Markus Thielker <markus@thielker.dev>
2025-04-08 10:58:03 +0200

    NORY-46: protect create-client page with permissions

--- dashboard/src/app/(inside)/client/create/page.tsx
+++ dashboard/src/app/(inside)/client/create/page.tsx
@@ -1,7 +1,19 @@
 import { CreateClientForm } from '@/components/forms/client-form';
 import { createClient } from '@/lib/action/client';
+import { checkPermission, requireSession } from '@/lib/action/authentication';
+import { permission, relation } from '@/lib/permission';
+import { redirect } from 'next/navigation';
 
 export default async function CreateClientPage() {
+
+    const session = await requireSession();
+    const identityId = session.identity!.id;
+
+    const pmCreateClient = await checkPermission(permission.client.it, relation.create, identityId);
+    if (!pmCreateClient) {
+        return redirect('/client');
+    }
+
     return (
         <div className="space-y-4">
             <div>
